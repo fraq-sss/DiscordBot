5f162994a37f929f60964d7bf224b2a5e8849d21
fraq-sss <98089449+fraq-sss@users.noreply.github.com>
2022-10-24 23:52:03 +0600

    Update bs_server.py

--- bs_server.py
+++ bs_server.py
@@ -6,26 +6,26 @@ with open('config.yaml') as cfg_file:
     cfg = full_load(cfg_file)['bs_servers']
 
 
-class Data:
-    name = tuple(cfg['folders'].keys())[0]
+name = tuple(cfg['folders'].keys())[0]
+players_stats_file = (
+    cfg['path'] +
+    cfg['folders'][name] +
+    cfg['players_stats_file']
+)
+
+
+def update_data(new_name: str) -> None:
+    global name, players_stats_file
+    name = new_name
     players_stats_file = (
         cfg['path'] +
         cfg['folders'][name] +
         cfg['players_stats_file']
     )
 
-    @staticmethod
-    def update(new_name: str) -> None:
-        name = new_name
-        players_stats_file = (
-            cfg['path'] +
-            cfg['folders'][name] +
-            cfg['players_stats_file']
-        )
-
 
 def get_players_stats() -> dict:
-    with open(Data.players_stats_file) as stats_file:
+    with open(players_stats_file) as stats_file:
         return loads(stats_file.read())
 
 
